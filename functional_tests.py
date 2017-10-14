b6850934ce5c9c3199061673ac7dce5535088b65
John Munsey <see3dee@gmail.com>
2017-10-14 09:11:25 -0400

    commit through pycharm...

--- functional_tests.py
+++ functional_tests.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 
-browser = webdriver.Firefox()
+browser = webdriver.Chrome()
 browser.get('http://localhost:8000')
-assert 'Django' in browser.title
+print(browser.title)
+assert 'Welcome to Django' in browser.title
