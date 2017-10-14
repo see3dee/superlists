3636581f956dcc1db1d2db9098d3b2d8dbe257e5
John Munsey <see3dee@gmail.com>
2017-10-14 14:50:08 -0400

    added unitest stuff

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,27 @@
 from selenium import webdriver
+import unittest
+import time
 
-browser = webdriver.Chrome()
-browser.get('http://localhost:8000')
-print(browser.title)
-assert 'Welcome to Django' in browser.title
+class NewVisitorTest(unittest.TestCase):
+
+    def setUp(self):
+        self.browser = webdriver.Chrome()
+        self.browser.implicitly_wait(3)
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_start_and_retrieve_a_list_laster(self):
+        # Customer has heard about a cool new "to-do" web application
+        # and goes to check it out
+        self.browser.get('http://localhost:8000')
+
+        # she notices "to-do" mentioned in the title header
+        self.assertIn('Django',self.browser.title)
+        #self.fail('finish the test')
+
+        # she is invited to add a to-do item straight away
+        #self.assertIn('Add a new To-do item?', self.browser.title)
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
