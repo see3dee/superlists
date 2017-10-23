812c174c25ff3daa52439b6fd478fb7f92b87b6f
John Munsey <see3dee@gmail.com>
2017-10-23 18:53:05 -0400

    make fail

--- functional_tests.py
+++ functional_tests.py
@@ -11,16 +11,16 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
-    def test_can_start_and_retrieve_a_list_laster(self):
+    def test_can_start_and_retrieve_a_list_later(self):
         # Customer has heard about a cool new "to-do" web application
         # and goes to check it out
         self.browser.get('http://localhost:8000')
 
         # she notices "to-do" mentioned in the title header
         print('the current title is ' + self.browser.title)
-        time.sleep(3)
+        self.assertIsNot('Lists',self.browser.title)
         self.assertIn('To-Do',self.browser.title)
-        #self.fail('finish the test')
+        self.fail('finish the test')
 
         # she is invited to add a to-do item straight away
         #self.assertIn('Add a new To-do item?', self.browser.title)
